7380709c9a4b2d06652e379d12ad9229c3bb1d3c
Spencer Ellinor <zpencerq@users.noreply.github.com>
2020-07-17 22:32:45 -0700

    Reduce dependencies

--- setup.py
+++ setup.py
@@ -11,10 +11,7 @@ setup(name='tap-framework',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_framework'],
       install_requires=[
-          'singer-python>=5.1.0,<5.2.0',
-          'backoff==1.3.2',
-          'requests==2.18.4',
-          'requests-oauthlib==0.8.0',
-          'funcy==1.10.1',
+          'singer-python>=5.8.0,<6.0.0',
+          'requests==2.24.0',
       ],
       packages=['tap_framework'])
